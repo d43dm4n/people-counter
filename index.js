b61ab09ea29b3b24304b23c09d8201e159fde43c
d34dm4n <szilveszter.pinter@gmail.com>
2023-02-13 20:53:24 +0100

    done first excersize

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+var countEL=document.getElementById("count-element");
+var saveEl=document.getElementById("saved-element");
 var count=0;
-console.log(count);
+function increment(){
+    count++;
+    countEL.textContent=count;
+}
+function save(){
+    let countString= count+" - ";
+    saveEl.textContent+=countString;
+    count=0;
+    countEL.textContent=count;
+}
